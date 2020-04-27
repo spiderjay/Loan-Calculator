4ade836395915cadf7005d0764d9e6565a41e905
Jay Phillips <jay@spiderjay.com>
2020-04-27 09:29:34 -0400

    Refactoring interest and monthly payment calculator.

--- calc.js
+++ calc.js
@@ -1,152 +1,134 @@
-function getInterest (credit, yov, term) {
-	var interest = '';
 
-  
-	if (credit <= 599 && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '11.99%';
-    } else if (600 <= credit && credit <= 649 && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '8.99%';
-    } else if (650 <= credit && credit <= 679 && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '6.49%';
-    } else if (680 <= credit && credit <= 724 && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '4.99%';
-    } else if (725 <= credit && credit <= 739 && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '4.49%';
-    } else if (740 <= credit && credit && 2010 <= yov && yov <= 2014 && term < 48) {
-        interest = '3.99%';
-    } else if (credit <= 599 && 2010 <= yov && yov <= 2014 && 49 <= term && term <= 60) {
-        interest = '12.99%';
-    } else if (
-        600 <= credit && credit <= 649 &&
-        2010 <= yov && yov <= 2014 &&
-        49 <= term && term <= 60
-    ) {
-        interest = '9.24%';
-    } else if (
-        650 <= credit && credit <= 679 &&
-        2010 <= yov && yov <= 2014 &&
-        49 <= term && term <= 60
-    ) {
-        interest = '6.99%';
-    } else if (
-        680 <= credit && credit <= 724 &&
-        2010 <= yov && yov <= 2014 &&
-        49 <= term && term <= 60
-    ) {
-        interest = '4.99%';
-    } else if (
-        725 <= credit && credit <= 739 &&
-        2010 <= yov && yov <= 2014 &&
-        49 <= term && term <= 60
-    ) {
-        interest = '4.49%';
-    } else if (740 <= credit && 2010 <= yov && yov <= 2014 && 49 <= term && term <= 60) {
-        interest = '3.99%';
-    } else if (credit <= 599 && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '11.99%';
-    } else if (600 <= credit && credit <= 649 && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '6.74%';
-    } else if (650 <= credit && credit <= 679 && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '6.24%';
-    } else if (680 <= credit && credit <= 724 && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '4.24%';
-    } else if (725 <= credit && credit <= 739 && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '3.49%';
-    } else if (740 <= credit && 2015 <= yov && yov <= 2016 && term <= 60) {
-        interest = '2.99%';
-    } else if (credit <= 599 && 2015 <= yov && yov <= 2016 && 61 <= term && term <= 72) {
-        interest = '12.49%';
-    } else if (
-        600 <= credit && credit <= 649 &&
-        2015 <= yov && yov <= 2016 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '6.99%';
-    } else if (
-        650 <= credit && credit <= 679 &&
-        2015 <= yov && yov <= 2016 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '6.49%';
-    } else if (
-        680 <= credit && credit <= 724 &&
-        2015 <= yov && yov <= 2016 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '4.24%';
-    } else if (
-        725 <= credit && credit <= 739 &&
-        2015 <= yov && yov <= 2016 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '3.49%';
-    } else if (740 <= credit && 2015 <= yov && yov <= 2016 && 61 <= term && term <= 72) {
-        interest = '2.99%';
-    } else if (credit <= 599 && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '10.49%';
-    } else if (600 <= credit && credit <= 649 && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '6.49%';
-    } else if (650 <= credit && credit <= 679 && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '5.99%';
-    } else if (680 <= credit && credit <= 724 && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '4.24%';
-    } else if (725 <= credit && credit <= 739 && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '3.49%';
-    } else if (740 <= credit && 2017 <= yov && yov <= 2020 && 0 <= term && term <= 60) {
-        interest = '2.99%';
-    } else if (credit <= 599 && 2017 <= yov && yov <= 2020 && 61 <= term && term <= 72) {
-        interest = '10.49%';
-    } else if (
-        600 <= credit && credit <= 649 &&
-        2017 <= yov && yov <= 2020 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '6.49%';
-    } else if (
-        650 <= credit && credit <= 679 &&
-        2017 <= yov && yov <= 2020 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '5.99%';
-    } else if (
-        680 <= credit && credit <= 724 &&
-        2017 <= yov && yov <= 2020 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '4.24%';
-    } else if (
-        725 <= credit && credit <= 739 &&
-        2017 <= yov && yov <= 2020 &&
-        61 <= term && term <= 72
-    ) {
-        interest = '3.49%';
-    } else if (740 <= credit && 2017 <= yov && yov <= 2020 && 61 <= term && term <= 72) {
-        interest = '2.99%';
-    } else if (
-        650 <= credit && credit <= 679 &&
-        2017 <= yov && yov <= 2020 &&
-        73 <= term && term <= 84
-    ) {
-        interest = '6.25%';
-    } 
-  
-    else if (
-        680 <= credit && credit <= 724 &&
-        2017 <= yov && yov <= 2020 &&
-        73 <= term && term <= 84
-    ) {
-        interest = '4.74%';
-    } else if (
-        725 <= credit && credit <= 739 &&
-        2017 <= yov && yov <= 2020 &&
-        73 <= term && term <= 84
-    ) {
-        interest = '3.74%';
-    } else if (740 <= credit && 2017 <= yov && yov <= 2020 && 73 <= term && term <= 84) {
-        interest = '3.49%';
-	}
+let rate = [];
+var years = [2017, 2015, 2010]; // define year thresholds
 
-	return interest;
+for (var i = 0; i < 6; i++) {
+    rate[i] = [];       // build credit tiers
+    for (y in years) {  
+        rate[i][years[y]] = []; // build years
+    }
+}
+
+// define rate matrix
+
+rate[0][2010][48] = 3.49;
+rate[0][2010][60] = 3.49;
+
+rate[0][2015][60] = 2.49;
+rate[0][2015][72] = 2.49;
+
+rate[0][2017][60] = 2.49;
+rate[0][2017][72] = 2.49;
+rate[0][2017][84] = 3.49;
+
+rate[1][2010][48] = 3.99;
+rate[1][2010][60] = 3.99;
+
+rate[1][2015][60] = 2.99;
+rate[1][2015][72] = 2.99;
+
+rate[1][2017][60] = 2.99;
+rate[1][2017][72] = 2.99;
+rate[1][2017][84] = 3.74;
+
+rate[2][2010][48] = 4.49;
+rate[2][2010][60] = 4.49;
+
+rate[2][2015][60] = 3.99;
+rate[2][2015][72] = 3.99;
+
+rate[2][2017][60] = 3.99;
+rate[2][2017][72] = 3.99;
+rate[2][2017][84] = 4.74;
+
+rate[3][2010][48] = 6.49;
+rate[3][2010][60] = 6.99;
+
+rate[3][2015][60] = 6.24;
+rate[3][2015][72] = 6.49;
+
+rate[3][2017][60] = 5.99;
+rate[3][2017][72] = 5.99;
+rate[3][2017][84] = 6.25;
+
+rate[4][2010][48] = 8.99;
+rate[4][2010][60] = 9.24;
+
+rate[4][2015][60] = 6.74;
+rate[4][2015][72] = 6.99;
+
+rate[4][2017][60] = 6.49;
+rate[4][2017][72] = 6.49;
+rate[4][2017][84] = 6.49;
+
+rate[5][2010][48] = 12.99;
+rate[5][2010][60] = 11.99;
+
+rate[5][2015][60] = 11.99;
+rate[5][2015][72] = 12.49;
+
+rate[5][2017][60] = 10.49;
+rate[5][2017][72] = 10.49;
+rate[5][2017][84] = 10.49;
+
+
+function getInterest(credit,yov,months) {
+
+    var c, y;
+
+    switch(true) {
+      case (credit >= 740):
+        c = 0;
+        break;
+      case (credit >= 725 && credit <= 739):
+        c = 1;
+        break;
+      case (credit >= 680 && credit <= 724):
+        c = 2;
+        break;  
+      case (credit >= 650 && credit <= 679):
+        c = 3;          
+        break;  
+      case (credit >= 600 && credit <= 649):
+        c = 4;
+        break;  
+      default:
+        c = 5;        
+        break;
+    }
+
+    switch(true) {
+        case (yov >= 2017):
+            y = 2017;
+            break;
+        case (yov >= 2015):
+            y = 2015;
+            break;  
+        case (yov >= 2010):
+            y = 2010;
+            break;  
+        default:
+            y = 2010;
+            break;              
+    }
+
+    var ms = 0;
+    rate[c][y].forEach(function(i,m) {
+        if (months > ms && months <= m) {
+            console.log("months is > "+ms+" and less than/eq to "+m);
+            console.log("The rate is "+rate[c][y][m]);
+        }
+        ms = m;
+    });
+
+    //return rate[c][y][m];
+
+}
+
+function getMontlyPayment(loan,rate,term){
+
+    var pay = ( ((loan * ((rate / 100) / 12) * ((1 +((rate / 100)/ 12)) ** term)/ (((1 + ((rate / 100)/ 12)) ** term) - 1))) );
+    return pay;
 }
 
 function calculator() {
@@ -154,14 +136,12 @@ function calculator() {
     var term = parseFloat($('#term').val());
     var credit = parseFloat($('#credit').val());
     var yov = parseFloat($('#yov').val());
-    var minterest = parseFloat(getInterest(credit, yov, term));
     var interest = parseFloat(getInterest(credit, yov, term));
+    var m_payment = (((loan * ((interest / 100) / 12) * ((1 +((interest / 100)/ 12)) ** term)/ (((1 + ((interest / 100)/ 12)) ** term) - 1))));
 
-    var m_payment = (((loan * ((minterest / 100) / 12) * ((1 +((minterest / 100)/ 12)) ** term)/ (((1 + ((minterest / 100)/ 12)) ** term) - 1))));
 
-
-   document.getElementById('output1').innerHTML = interest;
+    document.getElementById('output1').innerHTML = interest + '%';
     document.getElementById('output2').innerHTML = m_payment.toFixed(2);
     document.getElementById('output').classList.remove('hidden');
-};
+}
 
